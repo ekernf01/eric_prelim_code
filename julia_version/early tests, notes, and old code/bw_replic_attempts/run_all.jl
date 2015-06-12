95f0789e3414383e2ae29f89e5a05091c3bd31d7
Eric Kernfeld <eric.kern13@gmail.com>
2015-06-11 21:18:50 -0700

    created clean version to turn in
    removed big files with results and images

--- julia_version/early tests, notes, and old code/bw_replic_attempts/run_all.jl
+++
@@ -1,5 +0,0 @@
-include("big_test_bw0_05.jl")
-  include("big_test_bw0_3.jl")
-  include("big_test_bw0_1.jl")
-  include("big_test_bw_1.jl")
-
--- julia_version/early tests, notes, and old code/reload_modules.jl
+++
@@ -1,3 +0,0 @@
-#This allows Julia to find Eric's modules.
-workspace()
-push!(LOAD_PATH, "/Users/EricKernfeld/Desktop/Spring_2015/518/eric_prelim_code/julia_version/")
